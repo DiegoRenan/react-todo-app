9bebe6884e965198ff80123a0dfc98b3089c5ad7
DiegoRenan <renan.ccomp@gmail.com>
2019-05-22 18:30:01 -0300

    App Component

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,29 +11,29 @@ module.exports = {
         port: 8080,
         contentBase: './public',
     },
-    resolver: {
+    resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
     },
-    plugin: [
+    plugins: [
         new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
-            test: /js[x]?$/,
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
-                plugin: ['transform-object-rest-spread']
+                plugins: ['transform-object-rest-spread']
             }
         }, {
             test: /\.css$/,
-            loader: ExtractTextPluginExtract('style-loader', 'css-loader')
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            test: /\.woff|.woff2|.eot|.svg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }
