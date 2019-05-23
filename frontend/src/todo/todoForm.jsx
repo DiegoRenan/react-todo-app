af15ad09d290117c2e479ed78be345cfe030d809
DiegoRenan <renan.ccomp@gmail.com>
2019-05-23 15:46:21 -0300

    add input and button to TodoForm

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,12 +1,16 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class TodoForm extends Component {
-    
-    render() {
-        return(
-            <h1>
-                Form
-            </h1>
-        )
-    }
-}
+export default props => (
+    <div role='form' className='todoForm'>
+        <div className='col-xs-12 col-sm-9 col-md-10'>
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa'>
+            </input>
+        </div>
+
+        <div className='col-xs-12 col-sm-3 col-md-2'>
+            <button className='btn btn-primary'>
+                <i className='fa fa-plus'></i>
+            </button>
+        </div>        
+    </div>
+)
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default class TodoForm extends Component {
+    
+    render() {
+        return(
+            <h1>
+                List
+            </h1>
+        )
+    }
+}
