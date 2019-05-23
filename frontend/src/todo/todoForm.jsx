50d4cffce47b0e6ca4d191c00bcc63e9f1e66d46
DiegoRenan <renan.ccomp@gmail.com>
2019-05-23 15:32:54 -0300

    Components TodoForm and TodoList

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class TodoForm extends Component {
+    
+    render() {
+        return(
+            <h1>
+                Form
+            </h1>
+        )
+    }
+}
