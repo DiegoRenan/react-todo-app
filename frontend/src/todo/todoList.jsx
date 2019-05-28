138596ce55423d902014a3df53aad9d7eff030af
DiegoRenan <renan.ccomp@gmail.com>
2019-05-27 23:54:30 -0300

    improve CSS and Clear forms

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -21,6 +21,7 @@ export default props => {
             <thead>
                 <tr>
                     <th>Description</th>
+                    <th className='tableActions'>Ações</th>
                 </tr>
             </thead>
             <tbody>
